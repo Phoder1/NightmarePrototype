3c7b2320dcd72ccd10405a1f0b59cba557ea56f5
Phoder1 <alon.talmi@gmail.com>
2020-06-01 14:13:11 +0300

    BetterBoy+Background

---
+++ Assets/Assets/C#/ControlDimension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+namespace MoveTools {
+    [System.Serializable]
+    class RotationDimension {
+        [SerializeField]
+        internal Transform Transform;
+        [SerializeField]
+        internal float Coefficient;
+
+        internal float Angle;
+
+        [SerializeField]
+        internal float MinDegree;
+        [SerializeField]
+        internal float MaxDegree;
+        internal void UpdateAngle(float value) {
+            Angle += value * Coefficient * Time.deltaTime;
+            if (MinDegree != 0 || MaxDegree != 0) {
+                Angle = Mathf.Clamp(Angle, MinDegree, MaxDegree);
+            }
+
+        }
+    }
+
+    [System.Serializable]
+
+
+
+}
