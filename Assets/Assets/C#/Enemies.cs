11fc91e852af8931c7cf45a7aeb342c9c3100e41
Phoder1 <alon.talmi@gmail.com>
2020-06-06 19:55:24 +0300

    SwordRigidBody fix

--- Assets/Assets/C#/Enemies.cs
+++ Assets/Assets/C#/Enemies.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Enemies : MonoBehaviour
 {
+    [SerializeField]
+    bool IsFlying;
+    [SerializeField]
+    Transform[] targets;
+
+
     // Start is called before the first frame update
     void Start()
     {
