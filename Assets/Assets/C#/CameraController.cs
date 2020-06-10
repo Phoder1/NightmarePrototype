330cd52a3d8a9967070edba080061ce086c31628
Phoder1 <alon.talmi@gmail.com>
2020-06-11 02:17:32 +0300

    parallex

--- Assets/Assets/C#/CameraController.cs
+++ Assets/Assets/C#/CameraController.cs
@@ -1,28 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CameraController : MonoBehaviour
 {
+
     [SerializeField]
     float maxlevelX = Mathf.Infinity;
     [SerializeField]
     float maxlevelY = Mathf.Infinity;
     [SerializeField]
-    float speed;
+    float cameraSpeed;
     Transform playerTransform;
+
+    Vector3 movement;
     // Start is called before the first frame update
     void Start()
     {
+
         playerTransform = MainCharacterControls.mainCharacter.transform;
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         float posX = Mathf.Clamp(playerTransform.position.x, 0, maxlevelX);
         float posY = Mathf.Clamp(playerTransform.position.y, 0, maxlevelY);
-
-        transform.position = Vector3.MoveTowards(transform.position, new Vector3(posX, posY, transform.position.z), speed * Time.deltaTime);
+        
+        movement = (Vector3.MoveTowards(transform.position, new Vector3(posX, posY, transform.position.z), 1f) - transform.position) * cameraSpeed * Time.deltaTime;
+        transform.position += movement;
     }
 }
---
+++ Assets/Assets/C#/Parallex.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallex : MonoBehaviour
+{
+    [SerializeField]
+    float layerSpeed;
+    [SerializeField]
+    Transform cam;
+
+
+    Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(startPos.x + cam.position.x * layerSpeed , startPos.y + cam.position.y * layerSpeed , transform.position.z);
+    }
+}
