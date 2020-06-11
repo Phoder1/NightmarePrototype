cb62fa9cdb05deb34b1c132931b1ccbdd27e33c1
Phoder1 <alon.talmi@gmail.com>
2020-06-11 21:59:11 +0300

    platfrom collider bounds

---
+++ Assets/Assets/C#/PlatformBounds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformBounds : MonoBehaviour {
+    Collider2D collider;
+    // Start is called before the first frame update
+    void Start() {
+        collider = GetComponent<Collider2D>();
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireCube(collider.bounds.center, collider.bounds.size);
+    }
+}
