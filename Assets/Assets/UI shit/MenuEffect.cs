360333c5426d38a04fd0c1d17c11fbc16d0a27dc
JinMoshchenko <jinmoshchenko@gmail.com>
2020-06-12 23:50:57 +0300

    C# didn't know

--- Assets/Assets/UI shit/MenuEffect.cs
+++ Assets/Assets/UI shit/MenuEffect.cs
@@ -5,14 +5,51 @@ using UnityEngine;
 public class MenuEffect : MonoBehaviour
 {
 
-    private float nextActionTime = 0.0f;
-    public float period = 1f;
+    CanvasRenderer Renderer;
+
+    private float nextActionTime = 1f;
+    private float blinkTimeStart;
+    [SerializeField]
+    private float Duration = 0.2f;
+    [SerializeField]
+    private float period = 2.5f;
+    private int IsBlink = 0;
+    [SerializeField]
+    int BlinksCount = 2;
+    Color color1;
+    Color color2;
+
+    private void Start()
+    {
+        Renderer = GetComponent<CanvasRenderer>();
+        color1 = new Color(1f, 1f, 1f, 0f);
+        color2 = new Color(1f, 1f, 1f, 1f);
+    }
 
     void Update()
     {
-        if (Time.time > nextActionTime)
+        if (IsBlink == 0)
         {
-            nextActionTime += period;
+            if (Time.time > nextActionTime)
+            {
+
+                IsBlink++;
+                blinkTimeStart = Time.time;
+                Renderer.SetColor(color1);
+            }
+        }
+        else
+        {
+            if(Time.time>blinkTimeStart+Duration)
+            {
+                IsBlink++;
+                if(IsBlink >= BlinksCount)
+                {
+                    IsBlink = 0;
+                }
+                nextActionTime = Time.time + period;
+                Renderer.SetColor(color2);
+            }
         }
     }
 }
