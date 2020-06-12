582b5e4f127b735247005b79e8ece21d6a28a2c2
JinMoshchenko <jinmoshchenko@gmail.com>
2020-06-12 23:19:44 +0300

    menu

---
+++ Assets/Assets/UI shit/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public float wait_Play;
+
+    public void PlayGame()
+    {
+        Invoke("InvokeGame", wait_Play);
+    }
+
+    public void InvokeGame()
+    {
+        SceneManager.LoadScene("LevelDesignPrototype");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT works!!!");
+        Application.Quit();
+    }
+}
---
+++ Assets/Assets/UI shit/MenuEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuEffect : MonoBehaviour
+{
+
+    private float nextActionTime = 0.0f;
+    public float period = 1f;
+
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+        }
+    }
+}
