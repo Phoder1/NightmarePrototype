4cd3e9987433b43385e076922cff790c51aa62dc
JinMoshchenko <jinmoshchenko@gmail.com>
2020-06-13 00:59:29 +0300

    fddffdsfd

--- Assets/Assets/UI shit/MainMenu.cs
+++ Assets/Assets/UI shit/MainMenu.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
 
+    [SerializeField]
+    GameObject background;
+    Material BGmaterial;
     public float wait_Play;
 
+
+    void Start()
+    {
+        BGmaterial = background.GetComponent<Image>().material;
+        BGmaterial.SetFloat("DissolveIntensity", 0f);
+    }
     public void PlayGame()
     {
         Invoke("InvokeGame", wait_Play);
@@ -15,6 +25,7 @@ public class MainMenu : MonoBehaviour
 
     public void InvokeGame()
     {
+        BGmaterial.SetFloat("DissolveIntensity", 0f);
         SceneManager.LoadScene("LevelDesignPrototype");
     }
 
