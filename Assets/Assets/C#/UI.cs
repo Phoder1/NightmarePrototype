a0c9c43e62f5cf559e13a91a7ec9f42709c45678
Phoder1 <alon.talmi@gmail.com>
2020-06-14 15:19:28 +0300

    mainboy variant prefab

--- Assets/Assets/C#/UI.cs
+++ Assets/Assets/C#/UI.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class UI : MonoBehaviour
 {
     public static UI ui;
+
+    GameObject[] healthIcons;
+
     private void Awake() {
         if (ui != null && ui != this) {
             Destroy(this.gameObject);
@@ -16,7 +19,8 @@ public class UI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        healthIcons = new GameObject[transform.childCount];
     }
 
     // Update is called once per frame
